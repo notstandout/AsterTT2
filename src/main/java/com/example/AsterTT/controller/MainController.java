c4a5014db4028df890173cfacd7ae1938e8dfc6a
notstandout <meir_bek2000@mail.ru>
2023-09-14 12:43:23 +0600

    Commit from Meir last update

--- src/main/java/com/example/AsterTT/controller/MainController.java
+++ src/main/java/com/example/AsterTT/controller/MainController.java
@@ -1,2 +1,46 @@
-package com.example.AsterTT.controller;public class MainController {
+package com.example.AsterTT.controller;
+
+
+import com.example.AsterTT.dto.TasksDTO;
+import com.example.AsterTT.service.TasksService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/main")
+public class MainController {
+
+    @Autowired
+    private TasksService tasksService;
+
+
+    @GetMapping("/getAll")
+    private ResponseEntity<List<Object>> getAll(){
+        List<Object> tasksDTOS = tasksService.getAll().stream()
+                .map(task -> new TasksDTO(task.getId(), task.getTitle(), task.getDescription(),task.isDone()))
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(tasksDTOS, HttpStatus.OK);
+    }
+    @GetMapping("/getAllDoneTrue")
+    private ResponseEntity<List<Object>> getAllDoneTrue(){
+        List<Object> tasksDTOS = tasksService.getAllByDoneIsTrue().stream()
+                .map(task -> new TasksDTO(task.getId(), task.getTitle(), task.getDescription(),task.isDone()))
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(tasksDTOS, HttpStatus.OK);
+    }
+    @GetMapping("/getAllDoneFalse")
+    private ResponseEntity<List<Object>> getAllDoneFalse(){
+        List<Object> tasksDTOS = tasksService.getAllByDoneIsFalse().stream()
+                .map(task -> new TasksDTO(task.getId(), task.getTitle(), task.getDescription(),task.isDone()))
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(tasksDTOS, HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/AsterTT/dto/TasksDTO.java
+++ src/main/java/com/example/AsterTT/dto/TasksDTO.java
@@ -1,2 +1,21 @@
-package com.example.AsterTT.dto;public class TasksDTO {
+package com.example.AsterTT.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TasksDTO {
+
+    private int id;
+
+    private String title;
+
+    private String description;
+
+    private boolean done;
+
 }
